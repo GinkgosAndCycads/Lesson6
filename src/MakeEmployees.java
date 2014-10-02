5033cf559fef97bac6a40f2f53801c7a3f5af6ad
arsh3560 <arsh3560@ISS147D8-17.scdsb.pri>
2014-10-02 08:42:19 -0400

    Initial Commit

---
+++ src/MakeEmployees.java
@@ -0,0 +1,10 @@
+import TerminalIO.KeyboardReader;
+public class MakeEmployees{
+    public static void main(String[] args) {
+    KeyboardReader kr = new KeyboardReader(); 
+    String name = kr.readLine("Enter Name ---->: "); 
+    System.out.println("The Name is: "+name);
+            
+    }
+    
+}
